4b820fc48b59778585aa78a24cb10005a08794f8
binhlinh06 <nglinh150606@gmail.com>
2025-09-03 23:52:57 +0700

    ss21

--- PTIT_CNTT4_IT104_SESSION21/SS21/src/App.tsx
+++ PTIT_CNTT4_IT104_SESSION21/SS21/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 // import Baitap1 from './components/Ex1/Baitap1'
 // import Baitap2 from './components/Ex2/Baitap2'
 // import Baitap3 from './components/Ex3/Baitap3'
-// import Baitap4 from './components/Ex4/Baitap4'
-// import Baitap5 from './components/Ex5/Baitap5'
+import Baitap4 from './components/Ex4/Baitap4'
+import Baitap5 from './components/Ex5/Baitap5'
 // import Baitap6 from './components/Ex6/Baitap6'
-// import Baitap7 from './components/Ex7/Baitap7'
+import Baitap7 from './components/Ex7/Baitap7'
 // import Baitap8 from './components/Ex8/Baitap8'
 // import DefaultLayout from './layout/DefaultLayout'
 // import {Button} from 'antd'
@@ -23,10 +23,10 @@ export default function App() {
         {/* <Baitap1></Baitap1> */}
         {/* <Baitap2></Baitap2> */}
         {/* <Baitap3></Baitap3> */}
-        {/* <Baitap4></Baitap4> */}
-        {/* <Baitap5></Baitap5> */}
+        <Baitap4></Baitap4>
+        <Baitap5></Baitap5>
         {/* <Baitap6></Baitap6> */}
-        {/* <Baitap7> </Baitap7> */}
+        <Baitap7> </Baitap7>
         {/* <Baitap8> </Baitap8> */}
     </div>
   )
--- PTIT_CNTT4_IT104_SESSION21/SS21/src/components/Ex7/Baitap7.tsx
+++ PTIT_CNTT4_IT104_SESSION21/SS21/src/components/Ex7/Baitap7.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function Baitap7() {
   return (
-<div class="grid grid-cols-3 gap-6 p-6">
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">01</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">02</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">03</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">04</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">05</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">06</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">07</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">08</div>
-  <div class="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">09</div>
-</div>
-
-  )
+    <div className="grid grid-cols-3 gap-6 p-6">
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        01
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        02
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        03
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        04
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        05
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        06
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        07
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        08
+      </div>
+      <div className="bg-[rgb(217,70,239)] rounded-xl shadow-lg text-white font-bold flex items-center justify-center h-20">
+        09
+      </div>
+    </div>
+  );
 }
