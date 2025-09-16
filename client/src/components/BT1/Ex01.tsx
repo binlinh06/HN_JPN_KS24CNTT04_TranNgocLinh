cc3e418afd5f2388603f5d01291019eb8b15cc5c
binlinh06 <nglinh150606@gmail.com>
2025-09-16 07:57:24 +0700

    Delete client directory

--- client/src/components/BT1/Ex01.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Ex01() {
-  return (
-    <div>
-      Bài 1 ở db.json
-    </div>
-  )
-}
--- client/src/components/BT3/Ex03.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Ex03() {
-  return (
-    <div>
-      Bài 3 ở db.json
-    </div>
-  )
-}
